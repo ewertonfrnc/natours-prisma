bf816c9e0dca0bb467acdfc95dbd75985df76584
Ewerton Franco <ewefsilva@gmail.com>
2024-09-19 11:48:57 -0300

    feat: created tours and users resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ToursModule } from './tours/tours.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ToursModule, UsersModule],
 })
 export class AppModule {}
---
+++ src/tours/tours.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
+import { ToursService } from './tours.service';
+import { CreateTourDto } from './dto/create-tour.dto';
+import { UpdateTourDto } from './dto/update-tour.dto';
+
+@Controller('tours')
+export class ToursController {
+  constructor(private readonly toursService: ToursService) {}
+
+  @Post()
+  create(@Body() createTourDto: CreateTourDto) {
+    return this.toursService.create(createTourDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.toursService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.toursService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTourDto: UpdateTourDto) {
+    return this.toursService.update(+id, updateTourDto);
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.toursService.remove(+id);
+  }
+}
