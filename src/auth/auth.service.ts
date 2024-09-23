1b63793689128d9b97d90ac65997160d50ad42b8
Ewerton Franco <ewefsilva@gmail.com>
2024-09-22 22:57:16 -0300

    Add JWT support to AuthService
    Integrated JWT token generation in the AuthService, requiring a new ConfigService dependency. Updated package.json and package-lock.json to include @types/jsonwebtoken for type support.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { DatabaseService } from '../database/database.service';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'rxjs';
 
 @Injectable()
 export class AuthService {
-  constructor(private db: DatabaseService) {}
+  constructor(
+    private db: DatabaseService,
+    private config: ConfigService,
+  ) {}
 
   async create(createAuthDto: CreateAuthDto) {
-    const passwordHash = await bcrypt.hash(createAuthDto.password, 12);
-
     try {
+      const passwordHash = await bcrypt.hash(createAuthDto.password, 12);
+
       const user = await this.db.user.create({
         data: {
           name: createAuthDto.name,
@@ -24,9 +30,13 @@ export class AuthService {
         omit: { password: true, passwordConfirm: true },
       });
 
-      return { status: 'success', data: user };
+      const secret = this.config.get('JWT_SECRET');
+      const expiresIn = this.config.get('JWT_EXPIRES_IN');
+      const token = jwt.sign({ id: user.id }, secret, { expiresIn });
+
+      return { status: 'success', user, token };
     } catch (e) {
-      return { status: 'success', error: e };
+      return { status: 'fail', error: e.message };
     }
   }
 
